bce2b420e4093059f0c1b23ed702a358618866be
joelfuntes <joel_fuentes@yahoo.com>
2017-09-11 00:34:37 -0500

    vista para create blog formulario

---
+++ resources/views/blog/create.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+@section('content')
+<main class = "container">
+  <div class="container-fluid">
+    <div class="jumbotron">
+      <h1 align="center">crear blog post</h1>
+    </div>
+        <div class="col-sm-10 col-sm-offset-1">
+          {!! Form::open(['method' => 'POST']) !!}
+          <div class="form-group">
+            {!! Form::label('title' , 'Titulo:')!!}
+            {!! Form::text('title',null , ['class'=>'form-control'])!!}
+          </div>
+          <div class="form-group">
+            {!! Form::label('body' , 'Contenido:')!!}
+            {!! Form::textarea('body',null , ['class'=>'form-control'])!!}
+          </div>
+          <div class="form-group">
+            {!! Form::submit('Crear Blog', ['class'=>'btn btn-success'])!!}
+          </div>
+          {!! Form::close() !!}
+        </div>
+  </div>
+</main>
+@endsection
