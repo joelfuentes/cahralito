1bddd919237e76f31c5b19a3dc649e5bdd780907
joelfuntes <joel_fuentes@yahoo.com>
2017-09-11 00:32:27 -0500

    cambios en apariencia

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -1,14 +1,20 @@
 @extends('layouts.app')
 @section('content')
-<h1>Ultimos posts</h1>
-<hr>
-
-@foreach($blogs as $blog)
-
-  <h2>{{$blog->title}}</h2>
-  <p>{{$blog->body}}</p>
-
-  <h5>fecha de creacion:  {{$blog->created_at}}</h5>
-  <hr>
-@endforeach
+<main class = "container">
+  <div class="container-fluid">
+    <div class="jumbotron">
+      <h1 align="center">Ultimos posts</h1>
+    </div>
+        <div class="col-sm-8 col-sm-offset-2">
+            @foreach($blogs as $blog)
+            <article>
+                <h2>{{$blog->title}}</h2>
+                <p>{{$blog->body}}</p>
+                <h5>fecha de creacion:  {{$blog->created_at}}</h5>
+                <hr>
+            </article>
+            @endforeach
+        </div>
+  </div>
+</main>
 @endsection
