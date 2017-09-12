394519c38e6b406074dd6a325f85c0aded0971d8
joelfuntes <joel_fuentes@yahoo.com>
2017-09-12 16:44:53 -0500

    link para titles

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 @section('content')
-<main class = "container">
+<main class = "container-fluid">
   <div class="container-fluid">
     <div class="jumbotron">
       <h1 align="center">Ultimos posts</h1>
@@ -8,7 +8,7 @@
         <div class="col-sm-8 col-sm-offset-2">
             @foreach($blogs as $blog)
             <article>
-                <h2>{{$blog->title}}</h2>
+                <a href="#"><h2>{{$blog->title}}</h2></a>
                 <p>{{$blog->body}}</p>
                 <h5>fecha de creacion:  {{$blog->created_at}}</h5>
                 <hr>
