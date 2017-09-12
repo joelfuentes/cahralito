e592ba21cb0bb4b35c4d6eadf4f38df91d163606
joelfuntes <joel_fuentes@yahoo.com>
2017-09-12 16:45:37 -0500

    arreglando error de conflicto POST y GET->post en la ruta

--- resources/views/blog/create.blade.php
+++ resources/views/blog/create.blade.php
@@ -6,7 +6,7 @@
       <h1 align="center">crear blog post</h1>
     </div>
         <div class="col-sm-10 col-sm-offset-1">
-          {!! Form::open(['method' => 'POST']) !!}
+          {!! Form::open(['method' => 'POST', 'action'=>'BlogController@store']) !!}
           <div class="form-group">
             {!! Form::label('title' , 'Titulo:')!!}
             {!! Form::text('title',null , ['class'=>'form-control'])!!}
